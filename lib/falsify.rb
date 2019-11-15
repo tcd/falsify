de20119e3ee36bbc48701fa900883048ee70cf1b
Clay Dunston <claydunston@gmail.com>
2019-11-14 18:22:30 -0600

    Move version to it's own file

--- lib/falsify.rb
+++ lib/falsify.rb
@@ -1,6 +1,4 @@
 # Shopify resources.
-module Falsify
-  VERSION = "0.1.0".freeze
-end
+module Falsify; end
 
 Dir.glob(File.join(__dir__, "falsify", "/**/*.rb")).sort.each { |file| require file }
---
+++ lib/falsify/version.rb
@@ -0,0 +1,3 @@
+module Falsify
+  VERSION = "0.1.0".freeze
+end
